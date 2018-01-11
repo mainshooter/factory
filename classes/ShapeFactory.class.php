8ae271820d153518b1edd813eb8a6159f1a19cc3
Peter 4A5A <498883@edu.rocmn.nl>
2018-01-11 10:20:51 +0100

    Added check if a class exists

--- classes/ShapeFactory.class.php
+++ classes/ShapeFactory.class.php
@@ -1,8 +1,19 @@
 <?php
 
   class ShapeFactory {
+
     public static function create($className, $radius) {
-      return(new $className($radius));
+      if (self::checkIfClassExists($className)) {
+        return(new $className($radius));
+      }
+      return(false);
+    }
+
+    private function checkIfClassExists($className) {
+      if (file_exists('classes/' . $className . '.class.php') === true) {
+        return(true);
+      }
+      return(false);
     }
   }
 
