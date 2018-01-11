20eec81fa14cea41ed9a2c3665ccf99334158ef1
Peter 4A5A <498883@edu.rocmn.nl>
2018-01-11 09:58:39 +0100

    Added text

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
   require_once 'classes/ShapeFactory.class.php';
   require_once 'classes/Circle.class.php';
+  require_once 'classes/Square.class.php';
 
-  $Square = ShapeFactory::create('Circle', 3);
-  echo $Square->getArea();
+  $Circle = ShapeFactory::create('Circle', 3);
+  echo '<h1>Circle ' . $Circle->getArea() . '</h1>';
+
+  $Square = ShapeFactory::create('Square', 10);
+  echo "<h1>Square {$Square->getArea()}</h1>";
 ?>
